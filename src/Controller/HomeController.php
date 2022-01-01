be7dde120f7d275e03c5bf938aa282f805c83ab2
toyrik <toyrik@gmail.com>
2022-01-01 08:47:56 +0300

    FIX Исправляем аннотации для работы с php 7.4

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    /**
+     * @return Response
+     * @Route("/", name= "home")
+     */
     public function index(): Response
     {
         return $this->render('home.html.twig', [
