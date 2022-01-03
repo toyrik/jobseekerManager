02ce1ab773b857379f320a54405f8ccfc711323f
toyrik <toyrik@gmail.com>
2022-01-03 21:43:43 +0300

    Добавляем страницу добавления вакансии

--- src/Controller/Vacancy/VacancyController.php
+++ src/Controller/Vacancy/VacancyController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Vacancy;
 
 use App\ReadModel\Vacancy\VacancyFetcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Model\Vacancy\UseCase\Create;
 
 /**
  * @Route ("/vacancies")
@@ -28,8 +30,35 @@ class VacancyController extends AbstractController
     {
         $vacancies = $this->fetcher->all();
 
-        return $this->render('vacancies/index.html.twig',[
+        return $this->render('vacancy/index.html.twig',[
             'vacancies' => $vacancies,
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="users.create")
+     * @param Request $request
+     * @param Create\Handler $handler
+     * @return Response
+     */
+    public function create(Request $request, Create\Handler $handler): Response
+    {
+        $command = new Create\Command();
+
+        $form = $this->createForm(Create\Form::class, $command);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            try{
+                $handler->handle($command);
+                return $this->redirectToRoute('vacancies');
+            } catch (\DomainException $e) {
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('vacancy/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
